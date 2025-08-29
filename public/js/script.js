9205430abfbdb74dafb054c8ab169c984eb8877f
fadrian06 <franyeradriansanchez@gmail.com>
2025-08-29 16:48:21 -0400

    Remove isotope

--- public/js/script.js
+++ public/js/script.js
@@ -195,39 +195,6 @@
 		}
 	};
 
-	// init Isotope
-	var initIsotope = () => {
-		$(".grid").each(() => {
-			// $('.grid').imagesLoaded( function() {
-			// images have loaded
-			var $buttonGroup = $(".button-group");
-			var $checked = $buttonGroup.find(".is-checked");
-			var filterValue = $checked.attr("data-filter");
-
-			var $grid = $(".grid").isotope({
-				itemSelector: ".product-item",
-				layoutMode: "fitRows",
-				filter: filterValue,
-			});
-
-			// bind filter button click
-			$(".button-group").on("click", "a", function (e) {
-				e.preventDefault();
-				filterValue = $(this).attr("data-filter");
-				$grid.isotope({ filter: filterValue });
-			});
-
-			// change is-checked class on buttons
-			$(".button-group").each((i, buttonGroup) => {
-				$buttonGroup.on("click", "a", function () {
-					$buttonGroup.find(".is-checked").removeClass("is-checked");
-					$(this).addClass("is-checked");
-				});
-			});
-			// });
-		});
-	};
-
 	// init image zoom on single product page
 	var initImageZoom = () => {
 		$(".image-zoom")
@@ -273,8 +240,6 @@
 			$(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
 			$(this).toggleClass("animated");
 		});
-
-		initIsotope();
 	});
 
 	// document ready
